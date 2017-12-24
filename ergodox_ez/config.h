0ffdee416042e2456947c8eff3cdf440dcb1d215
Nic Aitch <nic@nicinabox.com>
2017-12-24 13:53:48 -0600

    Update ergodox config

--- ergodox_ez/config.h
+++ ergodox_ez/config.h
@@ -1,11 +1,6 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
-// #include "keyboards/ergodox_ez/config.h"
 #include "config_common.h"
-
-
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
 
 #endif
