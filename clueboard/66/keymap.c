b0b14d559aec4a5c49b9beb463458b51ce34f10c
Nic Aitch <nic@nicinabox.com>
2018-02-25 15:21:18 -0600

    Add clueboard windows layer

--- clueboard/66/keymap.c
+++ clueboard/66/keymap.c
@@ -1,10 +1,8 @@
 #include "66.h"
 
 #define QWERTY_LAYER 0
-#define F_LAYER 1
-
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
+#define WINDOWS_LAYER 1
+#define F_LAYER 10
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap QWERTY_LAYER: (Base Layer) Default Layer
@@ -25,7 +23,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,                   KC_MNXT, \
   CTL_T(KC_ESC), KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN,  KC_QUOT,  XXXXXXX,  KC_ENT,                             \
   KC_LSFT, XXXXXXX, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,   XXXXXXX, KC_RSFT,          KC_UP,            \
-  KC_CAPS, MO(F_LAYER), KC_LALT, KC_LGUI,   KC_SPC,   KC_SPC,  KC_RGUI,  KC_RALT,  MO(F_LAYER), KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT),
+  KC_CAPS, MO(F_LAYER), KC_LALT, KC_LGUI,   KC_SPC,   KC_SPC,  KC_RGUI,  KC_RALT,  MO(F_LAYER), TG(WINDOWS_LAYER),  KC_LEFT, KC_DOWN, KC_RGHT),
+
+
+[WINDOWS_LAYER] = KEYMAP(
+  KC_ESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,  KC_GRV,          KC_VOLU, \
+  KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,                   KC_VOLD, \
+  KC_LCTL, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN,  KC_QUOT,  XXXXXXX,  KC_ENT,                             \
+  KC_LSFT, XXXXXXX, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,   XXXXXXX, KC_RSFT,          KC_UP,            \
+  KC_LCTL, MO(F_LAYER), KC_LGUI, KC_LALT,   KC_SPC,  LT(F_LAYER, KC_SPC),  KC_RGUI,  KC_RALT,  MO(F_LAYER), _______,  KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Keymap F_LAYER: Function Layer
    * ,--------------------------------------------------------------------------.  ,----.
