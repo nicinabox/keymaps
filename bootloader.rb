94c68db82019076e7e1b6216079f0e54937b0cb0
Nic Aitch <nic@nicinabox.com>
2018-02-26 22:58:04 -0600

    Refactor bootloader.rb

--- bootloader.rb
+++ bootloader.rb
@@ -2,39 +2,28 @@
 
 KEYBOARD_PATH = ARGV[0]
 
-def parse_rules(line)
-  line and line.match(/=\s?(.*)/)[1]
-end
+FLASHERS = {
+  'lufa-dfu': 'dfu',
+  'qmk-dfu': 'dfu',
+  'atmel-dfu': 'dfu',
+  'caterina': 'avrdude',
+  'halfkay': 'teensy'
+}
 
-def parse_info_json(line)
-  line and line.match(/:\s?"(.*)"/)[1]
-end
+def find_bootloader
+  bootloaders = FLASHERS.keys
 
-def read_rules
-  File.open "#{KEYBOARD_PATH}/rules.mk" do |f|
-    f.find { |line| line =~ /BOOTLOADER\s?=/ }
+  ['rules.mk', 'info.json'].map do |filename|
+    result = bootloaders.find do |b|
+      pattern = Regexp.new b.to_s
+      File.foreach("#{KEYBOARD_PATH}/#{filename}").grep(pattern).any?
+    end
+    break result if result
   end
 end
 
-def read_info_json
-  File.open "#{KEYBOARD_PATH}/info.json" do |f|
-    f.find { |line| line =~ /"bootloader"/ }
-  end
+def flasher(bootloader)
+  FLASHERS[bootloader]
 end
 
-def bootloader
-  parse_rules(read_rules) or parse_info_json(read_info_json)
-end
-
-case bootloader
-when 'lufa-dfu'
-when 'qmk-dfu'
-when 'atmel-dfu'
-  puts 'dfu'
-
-when 'caterina'
-  puts 'avrdude'
-
-when 'halfkay'
-  puts 'teensy'
-end
+puts flasher(find_bootloader)
