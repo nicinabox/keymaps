1caee1621a21bffcec0c825eb3d496b1c0e6a3a0
Nic Haynes <nic@nicinabox.com>
2019-12-20 17:00:18 -0600

    Update iris config

--- keymaps/iris/config.h
+++ keymaps/iris/config.h
@@ -18,18 +18,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
-#include "config_common.h"
-
 /* Use I2C or Serial, not both */
-
 #define USE_SERIAL
 // #define USE_I2C
 
 /* Select hand configuration */
-
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
-// #define EE_HANDS
+#define EE_HANDS
 
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
