373bfd4e7693d4205991c381b8a56eb442b9309a
Nic Haynes <nic@nicinabox.com>
2019-12-23 23:51:43 -0600

    Make toggling to windows layer less accidental

--- keymaps/bananasplit/keymap.c
+++ keymaps/bananasplit/keymap.c
@@ -10,8 +10,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSLS, KC_DEL,
     HYPER_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_LBRC, KC_RBRC,       KC_BSPC,
     CTL_ESC,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,  KC_QUOT, KC_ENT,
-    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,  RSFT_SLSH,  KC_UP, DF(_WINDOWS),
+    KC_LSFT,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,  KC_DOT,  RSFT_SLSH,     KC_UP,   LOWER,
     KC_CAPS,  KC_LALT,  KC_LGUI,  KC_SPC,      RAISE,      KC_SPC,  KC_LGUI, KC_RALT,           KC_LEFT, KC_DOWN, KC_RIGHT
+  ),
+
+  [_RAISE] = LAYOUT_hhkb_arrow(
+    MO(_ADJUST),  _______,  _______,  _______, SCR, SCR_SEL, SCR_SEL_CP,  _______,  KC_MPRV,   KC_MPLY,  KC_MNXT, KC_VOLD, KC_VOLU, _______, _______,
+    _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,   _______,   _______,   KC_UNDS,   KC_PLUS,   KC_LCBR,   KC_RCBR,   _______, _______, KC_DEL,
+    _______,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,   _______,   KC_MINS,   KC_EQL,   KC_LBRC, KC_RBRC,  _______, _______,
+    _______,  KC_F9,   KC_F10,   KC_F11,   KC_F12,   TMUX,   _______,   _______,    _______,     _______,  KC_PPLS,  KC_PGUP, KC_INS,
+    _______,  _______,  _______, _______,      _______,         _______,               _______, _______,    KC_HOME, KC_PGDN, KC_END
   ),
 
   [_WINDOWS] = LAYOUT_hhkb_arrow(
@@ -30,16 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  _______, _______,  _______,   _______,               _______, _______,             KC_HOME, KC_PGDN, KC_END
   ),
 
-  [_RAISE] = LAYOUT_hhkb_arrow(
-    MO(_ADJUST),  _______,  _______,  _______, SCR, SCR_SEL, SCR_SEL_CP,  _______,  KC_MPRV,   KC_MPLY,  KC_MNXT, KC_VOLD, KC_VOLU, _______, _______,
-    _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,   _______,   _______,   KC_UNDS,   KC_PLUS,   KC_LCBR,   KC_RCBR,   _______, _______, KC_DEL,
-    _______,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,   _______,   KC_MINS,   KC_EQL,   KC_LBRC, KC_RBRC,  _______, _______,
-    _______,  KC_F9,   KC_F10,   KC_F11,   KC_F12,   TMUX,   _______,   _______,    _______,     _______,  KC_PPLS,  KC_PGUP, KC_INS,
-    _______,  _______,  _______, _______,      _______,         _______,               _______, _______,    KC_HOME, KC_PGDN, KC_END
-  ),
-
   [_ADJUST] = LAYOUT_hhkb_arrow(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, TG(_WINDOWS), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     RESET,  _______,   _______,   _______,  _______,  _______,   _______,   _______,   _______,   _______,   _______,   _______, _______, _______,
     _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,  _______, _______,
     _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,  _______,  _______,  _______, _______,
