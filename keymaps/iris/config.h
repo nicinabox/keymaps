7550ef65f6a308079552e0cbe5830cdaf2426327
Nic Haynes <nic@nicinabox.com>
2019-12-24 15:57:47 -0600

    Update iris

--- keymaps/iris/config.h
+++ keymaps/iris/config.h
@@ -28,8 +28,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define EE_HANDS
 
 #undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 12
+
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
--- keymaps/iris/rules.mk
+++ keymaps/iris/rules.mk
@@ -1,6 +1,2 @@
 RGBLIGHT_ENABLE = yes
 BACKLIGHT_ENABLE = no
-
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
