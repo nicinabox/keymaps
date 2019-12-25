eef5d10b7825182e62cc94127ff8e4277adefafe
Nic Haynes <nic@nicinabox.com>
2019-12-25 16:51:47 -0600

    Update planck

--- keymaps/planck/config.h
+++ keymaps/planck/config.h
@@ -1,8 +1,6 @@
 #ifndef CONFIG_USER_H
 #define CONFIG_USER_H
 
-#include "../config.h"
-
 #define PREVENT_STUCK_MODIFIERS
 
 #define TAPPING_TERM 200
@@ -14,12 +12,5 @@
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 10
 #define RGBLIGHT_VAL_STEP 10
-
-#define randadd 53
-#define randmul 181
-#define randmod 167
-
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
 
 #endif
