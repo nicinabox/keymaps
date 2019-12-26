54c6065ebb4d6f47f58e3e7b91f38896cdb515a5
Nic Haynes <nic@nicinabox.com>
2019-12-26 14:09:03 -0600

    Update ergodox ez

--- keymaps/ergodox_ez/keymap.c
+++ keymaps/ergodox_ez/keymap.c
@@ -1,73 +1,32 @@
-#include QMK_KEYBOARD_H
-#include "debug.h"
-#include "action_layer.h"
-#include "version.h"
-
-#define DEFAULT 0
-#define NORMAN 1
-#define LOWER 2
-#define NUMPAD 3
-#define NAV 4
-
-#define CTL_ESC CTL_T(KC_ESC)
-#define TMUX LCTL(KC_B)
-#define HPR_TAB ALL_T(KC_TAB)
-#define ALT_TAB LALT_T(KC_TAB)
-#define GUI_DEL LT(KC_LGUI, KC_DEL)
-#define SCR LGUI(LSFT(KC_3))
-#define SCR_SEL LGUI(LSFT(KC_4))
-#define CP_SCR_SEL LGUI(LCTL(LSFT(KC_4)))
+#include "nic.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[DEFAULT] = LAYOUT_ergodox(
+[_QWERTY] = LAYOUT_ergodox(
   KC_EQUAL,           KC_1,     KC_2,     KC_3,     KC_4,   KC_5,   KC_ESC,
-  ALT_TAB,            KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,   MO(LOWER),
+  KC_TAB,             KC_Q,     KC_W,     KC_E,     KC_R,   KC_T,   LOWER,
   CTL_ESC,            KC_A,     KC_S,     KC_D,     KC_F,   KC_G,
   KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,   KC_B,   KC_GRAVE,
-  KC_CAPS,            _______,  KC_HYPR,  KC_LGUI,  KC_LALT,
+  KC_CAPS,            KC_MEH,  KC_HYPR,  KC_LGUI,  KC_LALT,
 
                                           KC_HOME,  KC_END,
                                                     KC_PGUP,
-                                KC_BSPACE, GUI_DEL, KC_PGDOWN,
+                                 KC_BSPC, KC_DEL, KC_PGDOWN,
 
 
-    TG(NORMAN),      KC_6,      KC_7,     KC_8,      KC_9,      KC_0,      KC_MINUS,
-    KC_LBRC,         KC_Y,      KC_U,     KC_I,      KC_O,      KC_P,      KC_RBRC,
-                     KC_H,      KC_J,     KC_K,      KC_L,      KC_SCOLON, KC_QUOTE,
-    MEH_T(KC_BSLS),  KC_N,      KC_M,     KC_COMMA,  KC_DOT,    KC_SLASH,  KC_RSHIFT,
-                                MO(NAV),  KC_LEFT,   KC_DOWN,    KC_UP,    KC_RIGHT,
+    _______,         KC_6,      KC_7,     KC_8,      KC_9,      KC_0,    KC_MINUS,
+    KC_LBRC,         KC_Y,      KC_U,     KC_I,      KC_O,      KC_P,    KC_RBRC,
+                     KC_H,      KC_J,     KC_K,      KC_L,      KC_SCLN, KC_QUOTE,
+    KC_BSLS,         KC_N,      KC_M,     KC_COMMA,  KC_DOT,    KC_SLSH, KC_RSFT,
+                                RAISE,    KC_LEFT,   KC_DOWN,   KC_UP,   KC_RIGHT,
 
-    KC_MEDIA_PLAY_PAUSE,  KC_MEDIA_NEXT_TRACK,
-    KC_AUDIO_VOL_UP,
-    KC_AUDIO_VOL_DOWN,  KC_ENT, KC_SPC
+    KC_MPLY,  KC_MNXT,
+    KC_VOLU,
+    KC_VOLD,  KC_ENT, KC_SPC
 ),
 
-[NORMAN] = LAYOUT_ergodox(
-  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  KC_Q,     KC_W,     KC_D,     KC_F,     KC_K,     _______,
-  _______,  KC_A,     KC_S,     KC_E,     KC_T,     KC_G,
-  _______,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     _______,
-  _______,  _______,  _______,  _______,  _______,
-
-                                          _______,  _______,
-                                                    _______,
-                                _______,   _______,  _______,
-
-
-    _______,  _______,  _______,  _______,  _______,  _______,   _______,
-    _______,  KC_J,     KC_U,     KC_R,     KC_L,     KC_SCOLON, _______,
-              KC_Y,     KC_N,     KC_I,     KC_O,     KC_H,      _______,
-    _______,  KC_P,     KC_M,     KC_COMMA, KC_DOT,   KC_SLASH,  _______,
-                        _______,  _______,  _______,  _______,   _______,
-
-    _______,  _______,
-    _______,
-    _______,  _______, _______
-),
-
-[LOWER] = LAYOUT_ergodox(
-  _______,          _______,  _______,  SCR,  SCR_SEL, CP_SCR_SEL,  _______,
+[_LOWER] = LAYOUT_ergodox(
+  _______,          _______,  _______,  SCR,  SCR_SEL, SCR_SEL_CP,                          _______,
   LGUI(KC_BSPACE),  _______,  _______,  _______,  _______,          _______,                _______,
   _______,          _______,  _______,  _______,  _______,          _______,                _______,
   _______,          _______,  _______,  _______,  _______,          _______,
@@ -89,7 +48,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______
 ),
 
-[NUMPAD] = LAYOUT_ergodox(
+[_RAISE] = LAYOUT_ergodox(
+  _______,  _______,_______,_______,_______,_______,_______,
+  _______,  _______,_______,_______,_______,_______,_______,
+  _______,_______,_______,_______,_______,_______,
+  _______,_______,_______,_______,_______,_______,_______,
+  _______,_______,_______,_______,_______,
+
+                        _______,  _______,
+                                  _______,
+                _______, _______, _______,
+
+
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+  _______,  KC_PGUP,  KC_HOME,  KC_UP,    KC_END,   _______,  _______,
+            KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RIGHT, _______,  _______,
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                      _______,  _______,  _______,  _______,  _______,
+
+  KC_MPRV,_______,
+  _______,
+  KC_MUTE,_______,_______
+),
+
+[_NUMPAD] = LAYOUT_ergodox(
   _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,_______,
   _______,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,_______,
   _______,KC_F11 ,KC_F12 ,_______,_______,_______,
@@ -111,9 +93,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______
 ),
 
-[NAV] = LAYOUT_ergodox(
-  _______,  _______,_______,_______,_______,_______,RESET,
+[_ADJUST] = LAYOUT_ergodox(
   _______,  _______,_______,_______,_______,_______,_______,
+  RESET,  _______,_______,_______,_______,_______,_______,
   _______,_______,_______,_______,_______,_______,
   _______,_______,_______,_______,_______,_______,_______,
   _______,_______,_______,_______,_______,
@@ -124,28 +106,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   _______,  _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  KC_PGUP,  KC_HOME,  KC_UP,    KC_END,   _______,  _______,
-            KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RIGHT, _______,  _______,
+  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+            _______,  _______,  _______,  _______,  _______,  _______,
   _______,  _______,  _______,  _______,  _______,  _______,  _______,
                       _______,  _______,  _______,  _______,  _______,
 
-  KC_MPRV,_______,
+  _______,_______,
   _______,
-  KC_MUTE,_______,_______
+  _______,_______,_______
 ),
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-    switch(id) {
-      case 0:
-        if (record->event.pressed) {
-          SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-        }
-        break;
-    }
-    return MACRO_NONE;
-};
 
 void matrix_scan_user(void) {
 
