dcb70629a6613a554081ea1f3d88de60a43a4ec0
Nic Haynes <nic@nicinabox.com>
2020-10-09 10:47:21 -0500

    Setup combos

--- keymaps/iris_rev4/config.h
+++ keymaps/iris_rev4/config.h
@@ -16,6 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
+#include "config.h"
 
 // #define USE_I2C
 #define EE_HANDS
---
+++ user/config.h
@@ -0,0 +1,4 @@
+#undef COMBO_TERM
+#define COMBO_TERM 175
+
+#define COMBO_COUNT 5
