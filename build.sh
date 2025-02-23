665a7654f74dea3bd0fe04d4fd63b3b28592e4a1
Nic Haynes <nic@nicinabox.com>
2025-02-23 01:49:48 -0600

    fix build script

--- build.sh
+++ build.sh
@@ -7,8 +7,14 @@ TARGET_KEYBOARD=${keyboards[$SOURCE_KEYBOARD]:-$SOURCE_KEYBOARD}
 KEYMAP=${2:-$USER}
 
 echo "Syncing userspace files $SOURCE_KEYBOARD -> $TARGET_KEYBOARD.."
+
+mkdir -p "$QMK_PATH/users/$KEYMAP"
+mkdir -p "$QMK_PATH/keyboards/$TARGET_KEYBOARD/keymaps/$KEYMAP"
+
 rsync -av --delete users/$KEYMAP/ "$QMK_PATH/users/$KEYMAP"
-rsync -av --delete keyboards/$SOURCE_KEYBOARD/ "$QMK_PATH/keyboards/$TARGET_KEYBOARD/keymaps/$KEYMAP"
+
+ROOT_KEYBOARD=$(echo $TARGET_KEYBOARD | sed 's|/rev[0-9]*$||')
+rsync -av --delete keyboards/$SOURCE_KEYBOARD/ "$QMK_PATH/keyboards/$ROOT_KEYBOARD/keymaps/$KEYMAP"
 
 echo "Building firmware for $TARGET_KEYBOARD:$KEYMAP..."
 cd "$QMK_PATH"
@@ -16,7 +22,7 @@ qmk compile -kb "$TARGET_KEYBOARD" -km "$KEYMAP"
 
 if [ $? -eq 0 ]; then
     echo "Build successful!"
-    echo "To flash, run: ./flash.sh $SOURCE_KEYBOARD"
+    echo "To flash, run: ./flash.sh keyboards/$SOURCE_KEYBOARD"
 else
     echo "Build failed!"
     exit 1
